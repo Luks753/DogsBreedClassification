8be248e527e4f8f43c57eb99f9b5a63660b8ea2d
Luks753 <lucas753159@hotmail.com.br>
2019-01-07 02:20:47 -0300

    Atualiza(lê a descrição do commit)
    AlgoMLP = nosso algoritmo
    MLPagain = mlp github que tá sendo usado como base
    MLPKerasTensor = arquivo pra teste das redes
    
    Não se preocupa em melhorar a predição, dá pra gnt usar aquele algoritmo, só adaptar pro nosso.
    
    A predição tá horrível, taxa de acerto de 20%, mas deixa assim msm, qualquer coisa a gnt fala com o professor

--- AlgoMLP.py
+++ AlgoMLP.py
@@ -1,17 +1,39 @@
-import pickle
 import numpy as np
 import os
 import glob
-from scipy import misc
+import matplotlib.pyplot as plt
+from keras.utils import to_categorical
 from PIL import Image
-from imageop import rgb2grey
-import csv
 import cv2
-
-#class layers:
+import MLPagain
+     
     
 
 class MLP():
+    def __init__(self, size_layers, act_funct='sigmoid', reg_lambda=0, bias_flag=True):
+        self.size_layers = size_layers
+        self.n_layers    = len(size_layers)
+        self.act_f       = act_funct
+        self.lambda_r    = reg_lambda
+        self.bias_flag   = bias_flag
+ 
+        self.initialize_weights()
+        
+    def initialize_weights(self):
+        self.weights = []
+        size_next_layers = self.size_layers.copy()
+        size_next_layers.pop(0)
+        for size_layer, size_next_layer in zip(self.size_layers, size_next_layers):
+            epsilon = 4.0 * np.sqrt(6) / np.sqrt(size_layer + size_next_layer)
+            if self.bias_flag:  
+                theta_tmp = epsilon * ( (np.random.rand(size_next_layer, size_layer + 1) * 2.0 ) - 1)
+            else:
+                theta_tmp = epsilon * ( (np.random.rand(size_next_layer, size_layer) * 2.0 ) - 1)                              
+            self.weights.append(theta_tmp)
+        print(self.weights)
+        return self.weights
+        
+    
     
     def logistic_function(self,x):
         return .5 * (1 + np.tanh(.5 * x))
@@ -19,25 +41,109 @@ class MLP():
     
     
 class preProcess():
+    
+    def __init__(self):
+        print ("ok")
 
     def processimg(self,path):
-        png = [];
+        png = []
         for image_path in sorted(glob.glob(r""+path+"")):
             img = cv2.imread(image_path).astype('uint8')
             I = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
             height, width = I.shape
             I = np.reshape(I,height*width)
             png.append(I)
-        
+
         return np.asarray(png)
         
         
     def modifyimg(self,path):
-        i = 0
+        i = 1
         for image_path in sorted(glob.glob(r""+path+"")):
             tm = Image.open(image_path).convert('LA')
-            tm = tm.resize((50,50))
+            tm = tm.resize((32,32))
             name = os.path.dirname(image_path)
             tm.save(name+str(i)+'.png')
             i = i+1
+
+if __name__ == "__main__":
+    
+    data_train = np.loadtxt('basetreino.txt', dtype=int, delimiter=',')
+    data_test = np.loadtxt('baseteste.txt', dtype=int, delimiter=',')
+    data_train_labels = np.loadtxt('labels_basetreino.txt', dtype=int, delimiter='\n')
+    data_test_labels = np.loadtxt('labels_baseteste.txt', dtype=int, delimiter='\n')
+    
+    print('Training data shape : ', data_train.shape, data_train_labels.shape)
+     
+    print('Testing data shape : ', data_test.shape, data_test_labels.shape)
+    
+    teste = data_train[0].reshape(32,32)
+    
+    # Find the unique numbers from the train labels
+    classes = np.unique(data_train_labels)
+    nClasses = len(classes)
+    print('Total number of outputs : ', nClasses)
+    print('Output classes : ', classes)
+    
+    n_examples = len(data_train_labels)
+    
+    plt.figure(figsize=[10,5])
+    
+    # Display the first image in training data
+    plt.subplot(121)
+    plt.imshow(teste[:,:], cmap='gray')
+    plt.title("Ground Truth : {}".format(teste))
+    
+    #plt.show()
+    #'''
+    
+    # Change the labels from integer to categorical data
+    train_labels_one_hot = to_categorical(data_train_labels)
+    test_labels_one_hot = to_categorical(data_test_labels)
+    
+    # Display the change for category label using one-hot encoding
+    print('Original label 0 : ', data_train_labels[0])
+    print('After conversion to categorical ( one-hot ) : ', train_labels_one_hot)
+        
+    mlp_classifier = MLPagain.Mlp(size_layers = [1024, 512, 5], 
+                                  reg_lambda  = 0,
+                                  bias_flag   = True)
+    print(mlp_classifier)
+    
+    epochs = 10
+    loss = np.zeros([epochs,1])
+    
+    for ix in range(epochs):
+        mlp_classifier.train(data_train, train_labels_one_hot, 1)
+        Y_hat = mlp_classifier.predict(data_train)
+        y_tmp = np.argmax(Y_hat, axis=1)
+        y_hat = classes[y_tmp]
+        
+        loss[ix] = (0.5)*np.square(y_hat - data_train_labels).mean()
+    
+    # Ploting loss vs iterations
+    plt.figure()
+    ix = np.arange(epochs)
+    plt.plot(ix, loss)
+    
+    # Training Accuracy
+    Y_hat = mlp_classifier.predict(data_train)
+    y_tmp = np.argmax(Y_hat, axis=1)
+    y_hat = classes[y_tmp]
+    
+    acc = np.mean(1 * (y_hat == data_train_labels))
+    print('Training Accuracy: ' + str(acc*100))
+    
+    # Test Accuracy
+    Y_hat = mlp_classifier.predict(data_test)
+    y_tmp = np.argmax(Y_hat, axis=1)
+    y_hat = classes[y_tmp]
+    
+    acc = np.mean(1 * (y_hat == data_test_labels))
+    print(y_hat)
+    print('Testing Accuracy: ' + str(acc*100))
+
+    
+    
+    
     
--- MLPKerasTensor.py
+++ MLPKerasTensor.py
@@ -1,9 +1,103 @@
 import keras
 import numpy as np
 import matplotlib.pyplot as plt
-import pickle
+import MLPagain as mlp
+import pickle,gzip
 from keras.utils import to_categorical
 from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+#'''
+# As 'mnist.pkl.gz' was created in Python2, 'latin1' encoding is needed to loaded in Python3
+with gzip.open('mnist.pkl.gz', 'rb') as f:
+    train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
+
+# Plot random examples
+examples = np.random.randint(10000, size=8)
+n_examples = len(examples)
+plt.figure()
+for ix_example in range(n_examples):
+    tmp = np.reshape(train_set[0][examples[ix_example],:], [28,28])
+    ax = plt.subplot(1,n_examples, ix_example + 1)
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
+    plt.title(str(train_set[1][examples[ix_example]]))
+    plt.imshow(tmp, cmap='gray')
+
+
+# Training data
+train_X = valid_set[0]
+train_y = valid_set[1]  
+print('Shape of training set: ' + str(train_X.shape))
+
+# change y [1D] to Y [2D] sparse array coding class
+n_examples = len(train_y)
+labels = np.unique(train_y)
+train_Y = np.zeros((n_examples, len(labels)))
+for ix_label in range(len(labels)):
+    # Find examples with with a Label = lables(ix_label)
+    ix_tmp = np.where(train_y == labels[ix_label])[0]
+    train_Y[ix_tmp, ix_label] = 1
+
+
+# Test data
+test_X = test_set[0]
+test_y = test_set[1] 
+print('Shape of test set: ' + str(test_X.shape))
+
+# change y [1D] to Y [2D] sparse array coding class
+n_examples = len(test_y)
+labels = np.unique(test_y)
+test_Y = np.zeros((n_examples, len(labels)))
+for ix_label in range(len(labels)):
+    # Find examples with with a Label = lables(ix_label)
+    ix_tmp = np.where(test_y == labels[ix_label])[0]
+    test_Y[ix_tmp, ix_label] = 1
+
+# Creating the MLP object initialize the weights
+mlp_classifier = mlp.Mlp(size_layers = [784, 25, 10, 10], 
+                         act_funct   = 'relu',
+                         reg_lambda  = 0,
+                         bias_flag   = True)
+print(mlp_classifier)
+
+# Training with Backpropagation and 400 iterations
+iterations = 100
+loss = np.zeros([iterations,1])
+
+for ix in range(iterations):
+    mlp_classifier.train(train_X, train_Y, 1)
+    Y_hat = mlp_classifier.predict(train_X)
+    y_tmp = np.argmax(Y_hat, axis=1)
+    y_hat = labels[y_tmp]
+    
+    loss[ix] = (0.5)*np.square(y_hat - train_y).mean()
+
+# Ploting loss vs iterations
+plt.figure()
+ix = np.arange(iterations)
+plt.plot(ix, loss)
+
+# Training Accuracy
+Y_hat = mlp_classifier.predict(train_X)
+y_tmp = np.argmax(Y_hat, axis=1)
+y_hat = labels[y_tmp]
+
+print(y_hat)
+acc = np.mean(1 * (y_hat == train_y))
+print('Training Accuracy: ' + str(acc*100))
+
+# Test Accuracy
+Y_hat = mlp_classifier.predict(test_X)
+y_tmp = np.argmax(Y_hat, axis=1)
+y_hat = labels[y_tmp]
+
+print(y_hat)
+print(test_y)
+acc = np.mean(1 * (y_hat == test_y))
+print('Testing Accuracy: ' + str(acc*100))#'''
 
 '''
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
@@ -34,7 +128,7 @@ plt.title("Ground Truth : {}".format(test_labels[0]))
 plt.show()'''
 
 
-#'''
+'''
 data_train = np.loadtxt('basetreino.txt', dtype=float, delimiter=',')
 data_test = np.loadtxt('baseteste.txt', dtype=float, delimiter=',')
 data_train_labels = np.loadtxt('labels_basetreino.txt', dtype=int, delimiter='\n')
@@ -60,9 +154,52 @@ plt.imshow(teste[:,:], cmap='gray')
 plt.title("Ground Truth : {}".format(teste))
 
 plt.show()
+
+
+# Change the labels from integer to categorical data
+train_labels_one_hot = to_categorical(data_train_labels)
+test_labels_one_hot = to_categorical(data_test_labels)
+
+# Display the change for category label using one-hot encoding
+print('Original label 0 : ', data_train_labels[0])
+print('After conversion to categorical ( one-hot ) : ', train_labels_one_hot[0])
+
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(np.prod(teste.shape),)))
+model.add(Dense(512, activation='relu'))
+model.add(Dense(nClasses, activation='softmax'))
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+history = model.fit(data_train, train_labels_one_hot, batch_size=256, epochs=1000, verbose=1, 
+                   validation_data=(data_test, test_labels_one_hot))
+
+
+plt.figure(figsize=[8,6])
+plt.plot(history.history['loss'],'r',linewidth=3.0)
+plt.plot(history.history['val_loss'],'b',linewidth=3.0)
+plt.legend(['Training loss', 'Validation Loss'],fontsize=18)
+plt.xlabel('Epochs ',fontsize=16)
+plt.ylabel('Loss',fontsize=16)
+plt.title('Loss Curves',fontsize=16)
+plt.show()
+
+plt.figure(figsize=[8,6])
+plt.plot(history.history['acc'],'r',linewidth=3.0)
+plt.plot(history.history['val_acc'],'b',linewidth=3.0)
+plt.legend(['Training Accuracy', 'Validation Accuracy'],fontsize=18)
+plt.xlabel('Epochs ',fontsize=16)
+plt.ylabel('Accuracy',fontsize=16)
+plt.title('Accuracy Curves',fontsize=16)
+plt.show()
+
+[test_loss, test_acc] = model.evaluate(data_test, test_labels_one_hot)
+print("Evaluation result on Test Data : Loss = {}, accuracy = {}".format(test_loss, test_acc))
+
+# Predict the most likely class
+print(model.predict_classes(data_test[[0],:]))
+
 #'''
-
-
 
 
 
