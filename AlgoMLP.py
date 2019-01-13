e42162ca7b7e9d301bd41c835ed6773683e41fd5
Luks753 <lucas753159@hotmail.com.br>
2019-01-13 13:00:01 -0300

    Quase lá

--- AlgoMLP.py
+++ AlgoMLP.py
@@ -6,9 +6,9 @@ from keras.utils import to_categorical
 from PIL import Image
 import cv2
 import MLPagain
-
+from prettytable import PrettyTable
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
      
-    
 
 class MLP():
     def __init__(self, size_layers, learning_rate=0.2, reg_lambda=0, bias_flag=True):
@@ -36,7 +36,7 @@ class MLP():
         return self.weights
         
     
-    def train(self, X, Y, iterations=400, reset=False):
+    def train2(self, X, Y, iterations=400, reset=False):
         '''
         Given X (feature matrix) and y (class vector)
         Updates the Theta Weights by running Backpropagation N tines
@@ -114,19 +114,7 @@ class MLP():
                 value = f(value)
                 new_input.append(value)
             X=np.asarray(new_input)
-        return new_input, X[j+1]
-
-            
-
-                    
-                
-
-
-
-
-        
-    
-    
+        return new_input, X[j+1]                   
     
 class preProcess():
     
@@ -149,23 +137,24 @@ class preProcess():
         i = 1
         for image_path in sorted(glob.glob(r""+path+"")):
             tm = Image.open(image_path).convert('LA')
-            tm = tm.resize((50,50))
+            tm = tm.resize((200,200))
             name = os.path.dirname(image_path)
             tm.save(name+str(i)+'.png')
             i = i+1
 
 if __name__ == "__main__":
     
+    
     '''
     pr = preProcess()
-    pr.modifyimg("F:\Teste 2\*\*.jpg")
-    I = pr.processimg("F:\Teste 2\*.png")
-    np.savetxt('pugsamoyedTESTE.txt', I.astype(int), fmt='%.0f',delimiter=',', newline='\n')
+    pr.modifyimg("F:\Testar\*\*.jpg")
+    I = pr.processimg("F:\Testar\*.png")
+    np.savetxt('basetesteAV.txt', I.astype(int), fmt='%.0f',delimiter=',', newline='\n')
 
     Dir2="nome"
     i=-1
     Labels = []
-    for imagepath in sorted(glob.glob(r"F:\Teste 2\*\*.jpg")):
+    for imagepath in sorted(glob.glob(r"F:\Testar\*\*.jpg")):
         Dir1 = os.path.dirname(imagepath)
         if(Dir1 != Dir2):
             Dir2 = os.path.dirname(imagepath)
@@ -175,21 +164,20 @@ if __name__ == "__main__":
     #fim do for
     
     Im = np.asarray(Labels)
-    np.savetxt('labels_pugsamoyedTESTE.txt', Im.astype(int), fmt='%.0f',delimiter=',', newline='\n')#'''
+    np.savetxt('labels_basetesteAV.txt', Im.astype(int), fmt='%.0f',delimiter=',', newline='\n')
+
     
     
-    
-    
-    data_train = np.loadtxt('basetreino.txt', dtype=int, delimiter=',')
-    #data_test = np.loadtxt('baseteste.txt', dtype=int, delimiter=',')
+'''
+    data_train = np.loadtxt('basetreinoAV.txt', dtype=int, delimiter=',')
+    data_test = np.loadtxt('basetesteAV.txt', dtype=int, delimiter=',')
     data_train_labels = np.loadtxt('labels_basetreino.txt', dtype=int, delimiter='\n')
-    #data_test_labels = np.loadtxt('labels_baseteste.txt', dtype=int, delimiter='\n')
+    data_test_labels = np.loadtxt('labels_basetesteAV.txt', dtype=int, delimiter='\n')
     
     print('Training data shape : ', data_train.shape, data_train_labels.shape)
-     
-    #print('Testing data shape : ', data_test.shape, data_test_labels.shape)
+    print('Testing data shape : ', data_test.shape, data_test_labels.shape)
     
-    #teste = data_train[0].reshape(50,50)
+    teste = data_train[0].reshape(200,200)
     
     # Find the unique numbers from the train labels
     classes = np.unique(data_train_labels)
@@ -199,31 +187,30 @@ if __name__ == "__main__":
     
     n_examples = len(data_train_labels)
     
-    '''plt.figure(figsize=[10,5])
+    plt.figure(figsize=[10,5])
     
     # Display the first image in training data
     plt.subplot(121)
     plt.imshow(teste[:,:], cmap='gray')
-    plt.title("Ground Truth : {}".format(teste))'''
+    plt.title("Exemplo de imagem usada")
     
     #plt.show()
-    #'''
+
     
     # Change the labels from integer to categorical data
     train_labels_one_hot = to_categorical(data_train_labels)
-    #test_labels_one_hot = to_categorical(data_test_labels)
+    test_labels_one_hot = to_categorical(data_test_labels)
     
     # Display the change for category label using one-hot encoding
-    print('Original label 0 : ', data_train)
+    print('Original label 0 : ', data_train_labels[0])
     print('After conversion to categorical ( one-hot ) : ', train_labels_one_hot[0])
         
-    mlp_classifier = MLP(size_layers = [1024, 512, 1], 
+    mlp_classifier = MLPagain.Mlp(size_layers = [40000, 2], 
                                   reg_lambda  = 0,
                                   bias_flag   = False)
-    print(mlp_classifier)
-    mlp_classifier.train(data_train, train_labels_one_hot)
-    '''
-    epochs = 1000
+    
+    
+    epochs = 500
     loss = np.zeros([epochs,1])
     
     for ix in range(epochs):
@@ -244,20 +231,90 @@ if __name__ == "__main__":
     y_tmp = np.argmax(Y_hat, axis=1)
     y_hat = classes[y_tmp]
     
-    print(y_hat)
     acc = np.mean(1 * (y_hat == data_train_labels))
+    error = np.mean(1 * (y_hat != data_train_labels))
+    print("---------------------------")
+    print('Training Error: ' + str(error*100))
     print('Training Accuracy: ' + str(acc*100))
+    
     
     # Test Accuracy
     Y_hat = mlp_classifier.predict(data_test)
     y_tmp = np.argmax(Y_hat, axis=1)
     y_hat = classes[y_tmp]
     
-    acc = np.mean(1 * (y_hat == data_test_labels))
-    print(y_hat)
-    print('Testing Accuracy: ' + str(acc*100))'''
+    acctest = np.mean(1 * (y_hat == data_test_labels))
+    errortest = np.mean(1 * (y_hat != data_test_labels))
+    print("-----------TEST INFO-----------")    
+    print('Test Error: ' + str(errortest*100))
+    print('Test Accuracy: ' + str(acctest*100))#'''
+    
+    true_class1 = 0
+    false_class1 = 0
+    true_class2 = 0
+    false_class2 = 0
 
     
+    for i in range(len(data_test_labels)):
+        if(data_test_labels[i]==0):
+            if(y_hat[i] == 0):
+                true_class1 = true_class1 + 1
+            else:
+                false_class1 = false_class1 + 1
+        else:
+            if(y_hat[i] == 1):
+                true_class2 = true_class2 + 1
+            else:
+                false_class2 = false_class2 + 1
     
+    
+    
+    
+    total_exemplos1 = true_class1 + false_class1
+    total_exemplos2 = true_class2 + false_class2
+    total_previsto1 = true_class1 + false_class2
+    total_previsto2 = true_class2 + false_class1
+    
+    prec_rot = true_class1/total_previsto1
+    prec_shi = true_class2/total_previsto2
+    rec_rot  = true_class1/total_exemplos1
+    rec_shi  = true_class1/total_exemplos2
+    f1_rot   = ((prec_rot * rec_rot)/(prec_rot + rec_rot)) * 2
+    f1_shi   = ((prec_shi * rec_shi)/(prec_shi + rec_shi)) * 2
+    f1_total = (f1_rot + f1_shi)/2
+    
+    print("F1 score(Total): ", f1_total)
+    print("---------------------------")
+    print("******Rottweiler******")    
+    print("Precision(Rottweiler): ", prec_rot)
+    print("Recall(Rottweiler): ", rec_rot)
+    print("F1 score(Rottweiler): ", f1_rot)
+    print("---------------------------")
+    print("******Shitzu******") 
+    print("Precision(Shitzu): ", prec_shi)
+    print("Recall(Shitzu): ", rec_shi)
+    print("F1 score(Shitzu): ", f1_shi)
+    print("---------------------------")
+    
+    
+    
+    
+    #Cria a tabela
+    x = PrettyTable(["Matriz de confusao", "Rottweiler", "Shitzu", "Total de elementos"])
+
+    #Alinha as colunas
+    x.align["Matriz de confusao"] = "l"
+    x.align["Rottweiler"] = "l"
+    x.align["Shitzu"] = "r"
+    x.align["Total de elementos"] = "r"
+
+    #Deixa um espaco entre a borda das colunas e o conteudo (default)
+    x.padding_width = 1
+    
+    x.add_row(["Rottweiler",true_class1,false_class1, total_exemplos1]) 
+    x.add_row(["Shitzu",false_class2,true_class2, total_exemplos2])
+    x.add_row(["Total previstos",total_previsto1, total_previsto2," "]) 
+    print(x)                
+
     
     
